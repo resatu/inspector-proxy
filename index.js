952aeeda041056d4096dc05377ac0d67e4f0b0a3
resatu <resatugur.ulu@gmail.com>
2024-09-21 11:27:18 +0800

    update port to 6000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = 6000;
 
 const headers = {
     "Authorization": process.env.ONE_INCH_AUTHORIZATION,
